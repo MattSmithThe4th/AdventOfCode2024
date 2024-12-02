0738d44d110745f3c26b4ba3b558a874ab93c273
MattSmithThe4th <70754046+MattSmithThe4th@users.noreply.github.com>
2024-12-02 20:56:46 +0100

    Day 1 solution

--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -7,15 +7,16 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("Write the number of day to get the solution of\n");
             var day = Console.ReadLine();
             var solutionExecutor = SolutionFactory.GetInstance(day);
             var input = IOHandler.GetInput("Day" + day);
 
-            var answer = solutionExecutor.ExecuteSolution(input);
+            var answer1 = solutionExecutor.ExecuteSolutionPartOne(input);
+            var answer2 = solutionExecutor.ExecuteSolutionPartTwo(input);
 
-            Console.WriteLine("The answer is: " + answer);
+            Console.WriteLine("The answer to part one is: " + answer1);
+            Console.WriteLine("The answer to part two is: " + answer2);
         }
     }
 }
--- AdventOfCode2024/Solutions/SolutionDay1.cs
+++ AdventOfCode2024/Solutions/SolutionDay1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AdventOfCode2024.Helpers;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +10,59 @@ namespace AdventOfCode2024.Solutions
 {
     public class SolutionDay1 : SolutionBase
     {
-        public override string ExecuteSolution(string[] lines)
+        public override string ExecuteSolutionPartOne(string[] lines)
         {
-            throw new NotImplementedException("Day1");
+            var list1 = new List<int>();
+            var list2 = new List<int>();
+            (list1, list2) = SplitInputIntoLists(lines);
+            
+            list1.Sort();
+            list2.Sort();
+
+            var comparisonResultList = new List<int>();
+
+            for (int i = 0; i < list1.Count(); i++)
+            {
+                var difference = Math.Abs(list1[i] - list2[i]);
+                comparisonResultList.Add(difference);
+            }
+
+            var total = comparisonResultList.Sum(x => x);
+
+
+
+            return total.ToString();
+        }
+
+        public override string ExecuteSolutionPartTwo(string[] lines)
+        {
+            var list1 = new List<int>();
+            var list2 = new List<int>();
+            (list1, list2) = SplitInputIntoLists(lines);
+
+            var similarityscoreList = new List<int>();
+            foreach (var number in list1)
+            {
+                var occurences = list2.Where(x => x == number).Count();
+                similarityscoreList.Add(number * occurences);
+            }
+
+            var total = similarityscoreList.Sum(x => x);
+
+            return total.ToString();
+        }
+
+        private (List<int>, List<int>) SplitInputIntoLists(string[] lines)
+        {
+            var list1 = new List<int>();
+            var list2 = new List<int>();
+            foreach(var line in lines)
+            {
+                var splitLine = line.Split(' ').Where(x => !x.IsNullOrWhitespace()).ToArray();
+                list1.Add(Convert.ToInt32(splitLine[0]));
+                list2.Add(Convert.ToInt32(splitLine[1]));
+            }
+            return (list1, list2);
         }
     }
 }
