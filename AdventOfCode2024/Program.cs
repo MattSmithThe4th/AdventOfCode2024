d3dd82202d4033c161f19274c549f228bf6b50f5
MattSmithThe4th <70754046+MattSmithThe4th@users.noreply.github.com>
2024-12-02 22:04:30 +0100

    Day 2 solution

--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -12,11 +12,24 @@ namespace AdventOfCode
             var solutionExecutor = SolutionFactory.GetInstance(day);
             var input = IOHandler.GetInput("Day" + day);
 
-            var answer1 = solutionExecutor.ExecuteSolutionPartOne(input);
-            var answer2 = solutionExecutor.ExecuteSolutionPartTwo(input);
-
-            Console.WriteLine("The answer to part one is: " + answer1);
-            Console.WriteLine("The answer to part two is: " + answer2);
+            try
+            {
+                var answer1 = solutionExecutor.ExecuteSolutionPartOne(input);
+                Console.WriteLine("The answer to part one is: " + answer1);
+            } 
+            catch(Exception e)
+            {
+                Console.WriteLine("Error was thrown for implementation 1 with message: " + e.Message);
+            }
+            try
+            {
+                var answer2 = solutionExecutor.ExecuteSolutionPartTwo(input);
+                Console.WriteLine("The answer to part two is: " + answer2);
+            } 
+            catch(Exception e)
+            {
+                Console.WriteLine("Error was thrown for implementation 2 with message: " + e.Message);
+            }
         }
     }
 }
--- AdventOfCode2024/Solutions/SolutionDay2.cs
+++ AdventOfCode2024/Solutions/SolutionDay2.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AdventOfCode2024.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,12 +11,77 @@ namespace AdventOfCode2024.Solutions
     {
         public override string ExecuteSolutionPartOne(string[] lines)
         {
-            throw new NotImplementedException("Day2");
+            var result = 0;
+            foreach (var line in lines)
+            {
+                var splitLine = line.Split(' ').Where(x => !x.IsNullOrWhitespace()).Select(x => Convert.ToInt32(x)).ToArray();
+                if(RecursiveSafetyCheck(splitLine, 0, splitLine[0] < splitLine[1]))
+                {
+                    result++;
+                }
+            }
+
+            return result.ToString();
         }
 
         public override string ExecuteSolutionPartTwo(string[] lines)
         {
-            throw new NotImplementedException();
+            var result = 0;
+            foreach(var line in lines)
+            {
+                var splitLine = line.Split(' ').Where(x => !x.IsNullOrWhitespace()).Select(x => Convert.ToInt32(x)).ToArray();
+                if(RecursiveSafetyCheck(splitLine, 0, splitLine[0] < splitLine[1]))
+                {
+                    result++;
+                }
+                else if(AreEnoughVersionsSafe(splitLine))
+                {
+                    result++;
+                }
+            }
+
+            return result.ToString();
+        }
+
+
+        private bool RecursiveSafetyCheck(int[] splitLine, int currentIndex, bool ascending)
+        {
+            if(currentIndex == splitLine.Length - 1)
+            {
+                return true;
+            }
+            var currentValue = splitLine[currentIndex];
+            var nextValue = splitLine[currentIndex + 1];
+
+            if (currentValue == nextValue)
+            {
+                return false;
+            }
+            if ((ascending && currentValue > nextValue) || (!ascending && currentValue < nextValue))
+            {
+                return false;
+            }
+            if (Math.Abs(currentValue - nextValue) > 3)
+            {
+                return false;
+            }
+            
+            return RecursiveSafetyCheck(splitLine, currentIndex + 1, ascending);
+        }
+
+        private bool AreEnoughVersionsSafe(int[] splitLine)
+        {
+            var success = 0;
+            for (int i = 0; i < splitLine.Length; i++)
+            {
+                var asList = splitLine.ToList();
+                asList.RemoveAt(i);
+                if (RecursiveSafetyCheck(asList.ToArray(), 0, asList[0] < asList[1]))
+                {
+                    success++;
+                }
+            }
+            return success != 0;
         }
     }
 }
